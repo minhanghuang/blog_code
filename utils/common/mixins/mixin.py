788e35bdcce824d962862d8aa43b296313cd7fba
Coxhuang <mhesat@outlook.com>
2019-11-02 00:01:07 +0800

    update

--- utils/common/mixins/mixin.py
+++ utils/common/mixins/mixin.py
@@ -31,41 +31,35 @@ class MyCreateModeMixin(CreateModel):
         update:
             Update a user.
     '''
-    authentication_classes = ()
-    permission_classes = ()
+
     msg_create = "创建成功"
     results_display = True # 是否显示序列化信息, 默认显示
 
 class MyDeleteModelMixin(DestroyModel):
-    authentication_classes = ()
-    permission_classes = ()
+
     msg_delete = "成功删除"
     lookup_field = "pk" # 主键
 
 class MyUpdateModelMixin(UpdateModel):
-    authentication_classes = ()
-    permission_classes = ()
+
     msg_update = "修改成功"
     lookup_field = "pk" # 主键
     results_display = True # 是否显示序列化信息, 默认显示
 
 class MyListModeMixin(ListModel):
-    authentication_classes = ()
-    permission_classes = ()
+
     pagination_class = MyPagination # 分页
     queryset = models.UserProfile.objects.all() # models
     msg_list = "成功获取列表数据"
 
 class MyRetrieveModelMixin(RetrieveModel):
-    authentication_classes = ()
-    permission_classes = ()
+
     msg_detail = "成功获取详细数据"
     lookup_field = "pk" # 主键
 
 class MyAPIView(APIViewModel):
 
-    authentication_classes = ()
-    permission_classes = ()
+
     msg_api = "Ok"
 
 
