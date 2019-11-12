a80ed921ccb1fb80882075136349a94a74d5ce5b
Coxhuang <mhesat@outlook.com>
2019-11-13 00:25:40 +0800

    update

--- app_article/views/api/update_article/update_article_viewset.py
+++ app_article/views/api/update_article/update_article_viewset.py
@@ -1,6 +1,6 @@
 from utils.common.mixins.mixin import MyUpdateModelMixin
 from app_article.views.api.update_article.update_article_serializer import UpdateArticleSerializer
-
+from app_article import models
 
 
 
@@ -12,5 +12,6 @@ class UpdateArticleViewSet(MyUpdateModelMixin):
     # authentication_classes = ()  # 验证
     # permission_classes = ()  # 权限
     msg_update = "保存成功" # 提示信息
+    queryset = models.Article.objects.all()
     results_display = True  # 是否显示序列化信息, 默认显示
     serializer_class = UpdateArticleSerializer # 序列化类
