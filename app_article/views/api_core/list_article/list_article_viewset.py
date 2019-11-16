67fc4c113c60e64691bc26127f690e4185caca8c
Coxhuang <mhesat@outlook.com>
2019-11-17 01:33:20 +0800

    update
    新增文章根据内容标题搜索

--- app_article/views/api_core/list_article/list_article_viewset.py
+++ app_article/views/api_core/list_article/list_article_viewset.py
@@ -2,6 +2,7 @@ from utils.common.mixins.mixin import MyListModeMixin
 from app_article.views.api_core.list_article.list_article_serializer import ListArticleSerializer
 from app_article import models
 from django_filters import rest_framework
+from rest_framework import filters
 from app_article.views.filter.article.list import GetArticleListFilter
 
 
@@ -17,6 +18,7 @@ class ListArticleViewSet(MyListModeMixin):
     # permission_classes = ()  # 权限
     queryset = models.Article.objects.all().order_by("-createdate") # 倒序
     msg_list = "查看博文列表" # 提示信息
-    filter_backends = (rest_framework.DjangoFilterBackend,)
+    filter_backends = (rest_framework.DjangoFilterBackend,filters.SearchFilter,)
     filter_class = GetArticleListFilter
+    search_fields = ('title', 'subtitle', 'content',)
     serializer_class = ListArticleSerializer # 序列化类
